a1279900da115d5fc60832e32b5143dfd8c7dfeb
Colin Kebaso <colinnyamiaka@gmail.com>
2026-01-15 09:42:14 +0300

    Added setter dependency injection

--- src/main/java/com/colin/ecommerce/OrderService.java
+++ src/main/java/com/colin/ecommerce/OrderService.java
@@ -1,4 +1,19 @@
 package com.colin.ecommerce;
 
 public class OrderService {
+
+    private PaymentService paymentService;
+
+// We can comment out the Constructor dependency injection and we use setter injection below
+//    public OrderService(PaymentService paymentService) {
+//        this.paymentService = paymentService;
+//    }
+
+    public void setPaymentService(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    public void placeOrder(){
+        paymentService.processPayment(10);
+    }
 }
