699ad99f3eb5d7d9a60b04f6f13b2dce6a97e58a
Colin Kebaso <colinnyamiaka@gmail.com>
2026-02-03 11:56:45 +0300

    Put and delete operations

--- ecommerce/src/main/java/com/colin/ecommerce/controller/ProductController.java
+++ ecommerce/src/main/java/com/colin/ecommerce/controller/ProductController.java
@@ -5,8 +5,10 @@ import com.colin.ecommerce.service.ProductService;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,5 +35,16 @@ public class ProductController {
    @PostMapping("/products")
     public void addProduct(@RequestBody Product prod){
         service.addProduct(prod);
+    }
+
+    @PutMapping("/products")
+    public void updateProduct(@RequestBody Product prod){
+        service.updateProduct(prod);
+    }
+
+    
+    @DeleteMapping("/products/{prodId}")
+    public void deleteProduct(@PathVariable int prodId){
+        service.deleteProduct(prodId);
     }
 }
